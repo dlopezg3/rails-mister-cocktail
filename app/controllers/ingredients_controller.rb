683243353247761885ca8db886af33f31a9e6e37
Daniel Lopez <dlopez.10g@gmail.com>
2018-02-23 15:20:10 +0000

    dasas

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,20 @@
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
